1162361e9fd1c965e9a8ff4adae6a5ce31ea5cfb
nazaninshahrad <nazanin.shahrad@gmail.com>
2021-11-03 08:05:35 -0400

    add class

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+    Assignment 5
+    {Your name here}
+*/
+
+$(document).ready(function(){
+    // your code here
+    class ContentCard {
+        constructor(id, title, description, category) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.category = category;
+        }
+
+        updateContent() {
+            console.log(`Hi! I'm ${this.title}`);
+        };
+
+       
+        };
+    }
+
+});
+
+
